dfc0878555d15fd4b1fe31fc95ed40fbf3c2cbb6
kyotobear <kiytobear@gmail.com>
2022-06-07 20:54:27 -0500

    c

--- variablesandloops.java
+++ variablesandloops.java
@@ -1,10 +1,26 @@
 
+
+
 /**
  * variablesandloops
  */
-public class variablesandloops {
+public class variables {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+        boolean isAnswer = true;
+        double measurementSize = 10.5;
+        int batteryPercent = 95;
+        long billion = 1_000_000_000_000l;
+        String phrase = "Hello";
+        byte size = 1;
+
+        // declaring and initializing are NOT the same things
+        // to declare simply state type and identifier(name).
+        int a;
+        // to assign or initialize, we use the assignment operator = 
+        int b = 20;
+
     }
 }
+
